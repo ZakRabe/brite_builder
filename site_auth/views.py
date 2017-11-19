71698e208b864cd10704865e799b200d788aace1
Zachary Rabe <zakrabe@gmail.com>
2017-11-19 07:11:19 +0000

    added backup of DB just in case. some light auth success/error messages. added flag to talents to bypass spell limit

--- site_auth/views.py
+++ site_auth/views.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.contrib import messages 
+from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from champs.models import Champ
@@ -14,6 +14,7 @@ from django.core.exceptions import ValidationError
 # Create your views here.
 def login_view(request):
     if request.user.is_authenticated():
+        messages.error(request, "Already logged in")
         return redirect('/')
     champs = Champ.objects.all().exclude(title="Shared");
     if request.method == "POST":
@@ -22,6 +23,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            messages.success(request, "Successfully logged in")
             return redirect('/')
         else:
             return render(request, 'auth/login.html', {"error": "Unable to login with this Username and Password","champs":champs})
@@ -30,21 +32,23 @@ def login_view(request):
 
 def logout_view(request):
     logout(request)
+    messages.success(request, "Successfully logged out")
     return redirect('/')
 
 def register(request):
     if request.user.is_authenticated():
+        messages.error(request, "Already logged in")
         return redirect('/')
     champs = Champ.objects.all().exclude(title="Shared");
     if request.method == "POST":
-        form = RegisterForm(request.POST)    
+        form = RegisterForm(request.POST)
         valid = form.is_valid()
         try:
             password = request.POST['password']
             validate_password(password)
         except ValidationError, e:
             form._errors['password'] = e
-        
+
         if valid is True:
             email = request.POST['email']
             username = request.POST['username']
@@ -53,5 +57,5 @@ def register(request):
             return redirect('/auth/login')
         return render(request, 'auth/register.html', {'form': form, "champs":champs})
     else:
-        form = RegisterForm()   
+        form = RegisterForm()
         return render(request, 'auth/register.html',  {'form':form,"champs":champs})
--- talents/models.py
+++ talents/models.py
@@ -8,16 +8,17 @@ class Talent(models.Model):
     title       = models.CharField(max_length=50)
     description = models.CharField(max_length=300)
     type        = models.ForeignKey('TalentType')
-    spell       = models.ForeignKey('spells.Spell', blank=True, null=True)
-    
+    spell       = models.ForeignKey('spells.Spell', verbose_name="Spell Image",blank=True, null=True)
+    no_limit    = models.BooleanField(verbose_name="Bypass spell count limit", default=False)
+
     class Meta:
         ordering = ['spell']
 
-    
+
     def __str__(self):
-        
+
         return self.spell.champ.title + " - " + self.title if self.spell is not None and self.spell.champ is not None else self.title
-        
+
     def to_json(self):
         return {
             'id': self.id,
@@ -25,15 +26,16 @@ class Talent(models.Model):
             'description': self.description,
             'type': self.type.to_json(),
             'spell': self.spell.to_json(),
+            "no_limit": self.no_limit,
         }
-        
+
 class TalentType(models.Model):
     title = models.CharField(max_length=15)
     color = models.CharField(max_length=7)
-    
+
     def __str__(self):
         return self.title
-    
+
     def to_json(self):
         return {
             'id': self.id,
@@ -45,10 +47,10 @@ class SpecialTalent(models.Model):
     # these talents will be used for multiple champs
     champ   = models.ForeignKey('champs.Champ')
     talent  = models.ForeignKey('Talent')
-    
+
     def __str__(self):
         return self.talent.title + " + " + self.champ.title
-        
+
     def to_json(self):
         return {
             'id': self.id,
